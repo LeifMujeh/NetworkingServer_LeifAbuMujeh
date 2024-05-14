818b7681614e8eece3262c704fdb3bb87d3b4931
LeifMujeh <Leif.abumujeh@elev.ga.ntig.se>
2024-05-14 14:30:49 +0200

    Skapar server

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ServerSocket server = null;
+        Socket client;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Defoult port number we are giung to use
+        int portNumber  = 4500;
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
+        }
+        // Create Server Side Socket
+        try {
+         Server = new ServerSocket(portNumber);
+        } catch (IOException ie){
+            System.out.println(!"Cannot Open Socket." + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is Created " + server);
+        // wait for the data from the client and reply
+        while (true) {
+            try {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+                //listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request is accepted...");
+                String clientHost = client.getInetAddress().get hostadress();
+                int clientPort =  client.getPort();
+                System.out.println("Client Host = " + clientHost + "Client Port = "  + clientPort);
+
+                // Read data  from Client
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message");
+
+            }
         }
     }
 }
